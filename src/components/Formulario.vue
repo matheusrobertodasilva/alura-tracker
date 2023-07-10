6a0f455a61024f544619fbff5b296771f0d2d2f5
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2023-07-10 13:15:17 -0300

    subindo arquivos

---
+++ src/components/Formulario.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div 
+            class="column is-8" 
+            role="form" 
+            aria-label="Formulário para criação de uma nova tarefa"
+            >
+                <input 
+                type="text" 
+                class="input" 
+                placeholder="Qual tarefa você deseja iniciar?"
+                v-model="descricao"
+                />
+            </div>
+            <div class="column">
+                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import Temporizador from './Temporizador.vue'
+
+export default defineComponent({
+    name:'FormularioI',
+    components:{
+        Temporizador
+    },
+    data(){
+        return {
+            descricao:''
+        }
+    },
+    methods:{
+        finalizarTarefa(tempoDeCorrido: number) : void {
+            console.log('tempo da tarefa', tempoDeCorrido)
+            console.log(`Descrição da tarefa ${this.descricao}`)
+            this.descricao = '';
+        }
+    }
+})
+</script>
