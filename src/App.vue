1f1fceae72dffdb3efb3b9fe343f3ae8649298e0
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2023-07-11 14:38:50 -0300

    criação de rotas; criação do componente projeto; alteração na barra latera, inserindo duas opções: tarefa, projetos

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,8 @@
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa"/>
-      <div class="list">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        <Box v-if="listaEstaVazia">
-          Você não esta muito produtivo hoje :(
-        </Box>
-      </div>
+      <router-view/>
+     <!--AQUI VAI A VIEW CORRESPONDENTE-->
     </div>
   </main>
 </template>
@@ -18,34 +13,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import Formulario from './components/Formulario.vue';
-import Box from './components/Box.vue';
-import Tarefa from './components/Tarefa.vue';
-import ITarefa from '../src/interfaces/ITarefa'
 
 export default defineComponent({
   name: 'App',
   components :{
     BarraLateral,
-    Formulario,
-    Tarefa,
-    Box
   },
   data() {
     return{
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
-  computed:{
-    listaEstaVazia () : boolean{
-      return this.tarefas.length === 0
-    }
-  },
   methods:{
-    salvarTarefa(tarefa: ITarefa){
-      this.tarefas.push(tarefa)
-    },
     trocarTema(modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,11 +1,29 @@
 <template>
     <header>
         <h1>
-            <img src="../assets/logo.png" alt="nada">
+            <img src="../assets/logo.png" alt="Alura Tracker">
         </h1>
-        <button class="button" @click="alterarTema">
-            {{textoBotao}}
-        </button>
+        <div class="has-text-centered">
+            <button class="button" @click="alterarTema">{{textoBotao}}</button>
+        </div>
+        <nav class="panel mt-5">
+            <ul>
+                <li>
+                    <router-link to="/" class="link">
+                        <i class="fas fa-tasks">
+                            tarefas
+                        </i>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/projetos" class="link">
+                        <i class="fas fa-project-diagram">
+                            Projetos
+                        </i>
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -51,5 +69,18 @@ header {
         height:auto;
         padding:2.5rem;
     }
+}
+
+.panel li {
+    margin: 8px 0;
+}
+.link {
+    color:#fff;
+}
+.link:hover {
+    color: #faf0ca;
+}
+.link-router-link-active{
+    color: #faf0ca
 }
 </style>
