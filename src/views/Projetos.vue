972e44ece4c391651e0cd7137000918e7f21c5e0
matheusrobertodasilva <matheusrobertodasilva16@gmail.com>
2023-07-12 08:57:44 -0300

    criação de novas rotas para a edição de projetos, implementação das rotas

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,29 +1,29 @@
 <template>
     <section class="projetos">
         <h1 class="title">Projetos</h1>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
-                <input 
-                    type="text" 
-                    class="input" 
-                    v-model="nomeDoProjeto" 
-                    id="nomeDoProjeto"
-                >
-            </div>
-            <div class="field">
-                <button class="button" type="submit">Salvar</button>
-            </div>
-        </form>
+        <router-link to="/projetos/novo" class="button">
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo Projeto</span>
+        </router-link>
         <table class="table is-fullwidth">
             <thead>
                 <th>ID</th>
                 <th>NOME</th>
+                <th>ações</th>
             </thead>
             <tbody>
                 <tr v-for="projeto in projetos" :key="projeto.id">
                     <td>{{ projeto.id }}</td>
                     <td>{{ projeto.nome }}</td>
+                    <td>
+                        <router-link :to="`/projetos/${projeto.id}`" class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+                            </span>
+                        </router-link>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -33,30 +33,12 @@
 <script lang="ts">
 import { computed, defineComponent } from 'vue';
 import { useStore } from '@/store/index'
-import IProjeto from '@/interfaces/IProjeto';
 
 export default defineComponent({
     name: 'ProjetosI',
-    data(){
-        return {
-            nomeDoProjeto: ''
-        };
-    },
-    methods: {
-        salvar(){
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto)
-            this.nomeDoProjeto = ''
-
-        },
-    },
     setup(){
         const store = useStore()
         return {
-            store,
             projetos: computed(() => store.state.projetos)
         }
     }
---
+++ src/views/Projetos/Formulario.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="projetos">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
+                <input 
+                    type="text" 
+                    class="input" 
+                    v-model="nomeDoProjeto" 
+                    id="nomeDoProjeto"
+                >
+            </div>
+            <div class="field">
+                <button class="button" type="submit">Salvar</button>
+            </div>
+        </form>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from '@/store/index'
+//import IProjeto from '';
+
+export default defineComponent({
+    name: 'FormularioI',
+    data(){
+        return {
+            nomeDoProjeto: ''
+        };
+    },
+    methods: {
+        salvar(){
+            // const projeto: IProjeto = {
+            //     nome: this.nomeDoProjeto,
+            //     id: new Date().toISOString()
+            // }
+            // this.projetos.push(projeto)
+            this.store.commit('ADICIONAR_PROJETO', this.nomeDoProjeto)
+            this.nomeDoProjeto = '';
+            this.$router.push('/projetos')
+
+        },
+    },
+    setup(){
+        const store = useStore()
+        return {
+            store
+        }
+    }
+})
+
+</script>
+
+<style>
+.projetos{
+    padding: 1.25rem;
+}
+</style>
